3846724bfb5c3fd209c02188f56c1000f33771e4
Quentin Crida <qcrida@gmail.com>
2020-09-24 07:35:52 +0100

    blog links updated

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -18,8 +18,9 @@ get_header(); ?>
       the_post(); ?>
       <div class="post-item">
         <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+       
         <div class="metabox">
-          <p>Posted by <?php the_author_posts_link();?> on <?php the_time('j F Y');?> in <?php echo get_the_category_list(',')?></p>
+          <p>Posted by <?php the_author_posts_link();?> on <?php the_time('Y/m/d');?> in <?php echo get_the_category_list(',')?></p>
         </div>
         <div class="generic-content">
         <?php  the_excerpt();?>
