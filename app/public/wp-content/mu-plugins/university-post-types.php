87c7990d252b34e2ad376cf8178aa0f7b526f8b2
Quentin Crida <qcrida@gmail.com>
2020-10-07 07:46:31 +0100

    custom post types displaying Events

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,8 @@
  <?php
  function university_post_types() {
     register_post_type('event', array(
+        'rewrite' => array('slug' => 'events'),
+        'has_archive' => true,
         'public' => true,
         'labels' => array(
             'name' => 'Events',
