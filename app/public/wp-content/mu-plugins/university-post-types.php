08d7b6933590dd387691ee31f5f764d2134bd527
Quentin Crida <qcrida@gmail.com>
2020-10-09 06:52:52 +0100

    Program detail added

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,5 +1,6 @@
  <?php
  function university_post_types() {
+     //Event Post type
     register_post_type('event', array(
         'show_in_rest' => true,
         'supports' => array('title', 'editor', 'excerpt'),
@@ -15,6 +16,23 @@
         ),
         'menu_icon' => 'dashicons-calendar-alt'
     ));
+    //Programe post type
+    register_post_type('program', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
+        'rewrite' => array('slug' => 'programs'),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards'
+    ));
+
 }
 
 add_action('init', 'university_post_types');
