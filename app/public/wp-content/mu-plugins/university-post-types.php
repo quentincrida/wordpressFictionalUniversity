50f70dbd9800d892b4a6b2648176fbc95d7b3433
Quentin Crida <qcrida@gmail.com>
2020-10-12 14:52:31 +0100

    featured images

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -36,7 +36,7 @@
     //Professor post type
     register_post_type('professor', array(
         'show_in_rest' => true,
-        'supports' => array('title', 'editor'),
+        'supports' => array('title', 'editor', 'thumbnail'),
         'public' => true,
         'labels' => array(
             'name' => 'Professors',
