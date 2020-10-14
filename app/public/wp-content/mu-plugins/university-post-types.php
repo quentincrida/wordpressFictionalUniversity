e013517ff39e6b7e5f2267df5d4197cb7a2efe56
Quentin Crida <qcrida@gmail.com>
2020-10-14 06:39:07 +0100

    google maps APIs added

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -48,6 +48,23 @@
         'menu_icon' => 'dashicons-welcome-learn-more'
     ));
 
+    //Campus post type
+    register_post_type('campus', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
+        'rewrite' => array('slug' => 'campuses'),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Campuses',
+            'add_new_item' => 'Add New Campus',
+            'edit_item' => 'Edit Campus',
+            'all_items' => 'All Campuses',
+            'singular_name' => 'Campus'
+        ),
+        'menu_icon' => 'dashicons-location-alt'
+    ));
+
 }
 
 add_action('init', 'university_post_types');
