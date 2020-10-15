32cf54cdcd7339cc4e34a69a72fefc76aba4a463
Quentin Crida <qcrida@gmail.com>
2020-10-15 15:47:02 +0100

    search and esc keyboard shortcuts created in javascript

--- app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -7,20 +7,37 @@ class Search {
        this.closeButton = $(".search-overlay__close");
        this.searchOverlay = $(".search-overlay");
        this.events();
+       this.isOverlayOpen = false;
     }
     //2. Events intitiated
     events() {
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
+        $(document).on("keydown", this.keyPressDispatcher.bind(this));
     }
 
     //3.methods (verbs)
     openOverlay() {
       this.searchOverlay.addClass("search-overlay--active");
+      $("body").addClass("body-no-scroll");
+      console.log("Our open method has run");
+      this.isOverlayOpen = true;
     }
 
     closeOverlay() {
       this.searchOverlay.removeClass("search-overlay--active");  
+      $("body").removeClass("body-no-scroll");
+      console.log("Our close method just ran");
+      this.isOverlayOpen = false;
+    }
+
+    keyPressDispatcher(e) {
+        if (e.keyCode == 83 && !this.isOverlayOpen) {
+            this.openOverlay();
+        }
+        if (e.keyCode == 27 && this.isOverlayOpen == true) {
+            this.closeOverlay();
+        }
     }
 }
 export default Search;
