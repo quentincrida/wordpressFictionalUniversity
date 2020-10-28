292d91ed5022f214522e71e3fbfd0b9bf6752f0c
Quentin Crida <qcrida@gmail.com>
2020-10-28 10:56:09 +0000

    asynchronous search for posts and pages

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -38,8 +38,8 @@ function university_files(){
         wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
     } else {
         wp_enqueue_script('our_vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.9678b4003190d41dd438.js'), NULL, '1.0', true);
-        wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.c3407a16868229cae7cd.js'), NULL, '1.0', true);
-        wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.c3407a16868229cae7cd.css'));
+        wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.8b9d6aff8ab6abc5789e.js'), NULL, '1.0', true);
+        wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.8b9d6aff8ab6abc5789e.css'));
     }
    
     wp_localize_script('main-university-js', 'universityData',  array(
