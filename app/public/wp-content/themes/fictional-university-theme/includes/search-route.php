d3aeba41e8546337c8680aa089cbee887f9333bd
Quentin Crida <qcrida@gmail.com>
2020-12-03 07:16:01 +0000

    WP Query and keyword search

--- app/public/wp-content/themes/fictional-university-theme/includes/search-route.php
+++ app/public/wp-content/themes/fictional-university-theme/includes/search-route.php
@@ -8,9 +8,10 @@ function universityRegisterSearch() {
         'callback' => 'universitySearchResults'
     ));
 }
-function universitySearchResults() {
+function universitySearchResults($data) {
     $professors = new WP_Query(array(
-        'post_type' => 'professor'
+        'post_type' => 'professor',
+        's' => sanitize_text_field($data['term'])
     ));
 
 $professorResults = array();
