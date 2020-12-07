d097fe7593a7a849b477ae7fcf7e9eb8a57ea28b
Quentin Crida <qcrida@gmail.com>
2020-12-07 06:28:28 +0000

    Programs now working in Search

--- app/public/wp-content/themes/fictional-university-theme/includes/search-route.php
+++ app/public/wp-content/themes/fictional-university-theme/includes/search-route.php
@@ -37,28 +37,28 @@ while($mainQuery->have_posts()) {
     if (get_post_type() == 'professor') {
         array_push($results['professors'], array(
             'title' => get_the_title(),
-            'url' => get_the_permalink()
+            'permalink' => get_the_permalink()
         ));
     }
 
     if (get_post_type() == 'program') {
         array_push($results['programs'], array(
             'title' => get_the_title(),
-            'url' => get_the_permalink()
+            'permalink' => get_the_permalink()
         ));
     }
 
     if (get_post_type() == 'event') {
         array_push($results['events'], array(
             'title' => get_the_title(),
-            'url' => get_the_permalink()
+            'permalink' => get_the_permalink()
         ));
     }
 
     if (get_post_type() == 'campus') {
         array_push($results['campuses'], array(
             'title' => get_the_title(),
-            'url' => get_the_permalink()
+            'permalink' => get_the_permalink()
         ));
     }
     
