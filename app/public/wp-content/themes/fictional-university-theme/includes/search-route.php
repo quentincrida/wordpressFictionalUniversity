7f972b277cf44179de5ee93e7f218de8a60dfb89
Quentin Crida <qcrida@gmail.com>
2020-12-08 07:13:58 +0000

    search logic hard coded for Biology in Professors

--- app/public/wp-content/themes/fictional-university-theme/includes/search-route.php
+++ app/public/wp-content/themes/fictional-university-theme/includes/search-route.php
@@ -70,12 +70,37 @@ while($mainQuery->have_posts()) {
     if (get_post_type() == 'campus') {
         array_push($results['campuses'], array(
             'title' => get_the_title(),
-            'permalink' => get_the_permalink(),
-            
+            'permalink' => get_the_permalink()
         ));
     }
     
 }
+
+$programRelationshipQuery = new WP_Query(array(
+    'post_type' => 'professor',
+    'meta_query' => array(
+        array(
+            'key' => 'related_programs',
+            'compare' => 'LIKE',
+            'value' => '"70"'
+        )
+    )
+));
+
+while($programRelationshipQuery->have_posts()) {
+    $programRelationshipQuery->the_post();
+
+    if (get_post_type() == 'professor') {
+        array_push($results['professors'], array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+        ));
+    }
+
+}
+
+$results['professors'] = array_values(array_unique($results['professors'],SORT_REGULAR));
 
 return $results;
 
