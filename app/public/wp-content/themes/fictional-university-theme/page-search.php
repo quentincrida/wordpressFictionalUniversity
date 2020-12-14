5d0befb3a7bd3098c00027eaa6b45f690c95b073
Quentin Crida <qcrida@gmail.com>
2020-12-14 10:57:00 +0000

    traditional WP search completed

--- app/public/wp-content/themes/fictional-university-theme/page-search.php
+++ app/public/wp-content/themes/fictional-university-theme/page-search.php
@@ -49,13 +49,7 @@ get_header();
         <?php } ?>      
 
     <div class="generic-content">
-    <form class="search-form" method="get" action="<?php echo  esc_url(site_url('/')); ?>">
-        <label class="headline headline--medium" for="s">Perform a New Search:</label>
-        <div class="search-form-row">
-            <input placeholder="What are you looking for?" class="s" id="s" type="search" name="s">
-            <input class="search-submit" type="submit" value="search">
-        </div>
-    </form>
+     <?php get_search_form(); ?>
     </div>
 
   </div>
