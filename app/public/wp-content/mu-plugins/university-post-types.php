a8ca70f93fae163af4378931b831ccbe57e72b51
Quentin Crida <qcrida@gmail.com>
2021-01-05 10:54:32 +0000

    My notes page added and started

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -69,6 +69,21 @@
         'menu_icon' => 'dashicons-location-alt'
     ));
 
+//Note post type
+    register_post_type('note', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
+        'public' => false,
+        'show_ui' => true,
+        'labels' => array(
+            'name' => 'Notes',
+            'add_new_item' => 'Add New Note',
+            'edit_item' => 'Edit Note',
+            'all_items' => 'All Notes',
+            'singular_name' => 'Note'
+        ),
+        'menu_icon' => 'dashicons-welcome-write-blog'
+    ));
 }
 
 add_action('init', 'university_post_types');
