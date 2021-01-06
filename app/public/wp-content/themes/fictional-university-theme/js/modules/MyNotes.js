a0c265cbe17fbe83d2ff77b53e17aedeb8f500ab
Quentin Crida <qcrida@gmail.com>
2021-01-06 11:52:39 +0000

    Edit POST route and Button Completed

--- app/public/wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ app/public/wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -8,6 +8,8 @@ class MyNotes {
     events() {
         $(".delete-note").on("click", this.deleteNote);
         $(".edit-note").on("click", this.editNote.bind(this));
+        $(".update-note").on("click", this.updateNote.bind(this));
+
     }
 
     //Methods go here
@@ -47,6 +49,33 @@ class MyNotes {
             type: 'DELETE',
             success: (response) => {
                 thisNote.slideUp();
+                console.log("Congrats");
+                console.log(response);
+            },
+            error: (response) => {
+                console.log("Sorry");
+                console.log(response);
+            }
+        });
+    }
+
+    updateNote(e) {
+        var thisNote = $(e.target).parents("li");
+
+        var ourUpdatedPost = {
+            'title': thisNote.find(".note-title-field").val(),
+            'content': thisNote.find(".note-body-field").val()
+        }
+
+        $.ajax({
+            beforeSend: (xhr) => {
+              xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+            },
+            url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
+            type: 'POST',
+            data: ourUpdatedPost,
+            success: (response) => {
+                this.makeNoteReadOnly(thisNote);
                 console.log("Congrats");
                 console.log(response);
             },
