8df025c19d0fbee8279046d1dce66cea87a2f8c5
Quentin Crida <qcrida@gmail.com>
2021-01-15 07:10:18 +0000

    Like post continued

--- app/public/wp-content/themes/fictional-university-theme/includes/like-route.php
+++ app/public/wp-content/themes/fictional-university-theme/includes/like-route.php
@@ -7,6 +7,7 @@ function universityLikeRoutes() {
         'methods' => 'POST',
         'callback' => 'createLike'
     ));
+
     register_rest_route('university/v1', 'manageLike', array(
         'methods' => 'DELETE',
         'callback' => 'deleteLike'
@@ -14,9 +15,20 @@ function universityLikeRoutes() {
 }
 
 
-function createLike() {
-    return "Thanks for CREATING";
-}
+function createLike($data) {
+    $professor = sanitize_text_field($data['professorId']);
+
+    wp_insert_post(array(
+        'post_type' => 'like',
+        'post_status' => 'publish',
+        'post_title' => 'Second PHP test',
+        'meta_input' => array(
+           'liked_professor_id' =>  $professor
+        )
+        
+
+    ));
+}    
 
 function deleteLike() {
     return "Thanks for DELETING";
